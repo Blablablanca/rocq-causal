0072f913d05e6dbf98d2e770bc65ddcffafef235
Blanca Luo <ql101@wellesley.edu>
2026-07-02 13:51:06 -0400

    node domain

--- Experiment/Exp_Basics.v
+++ Experiment/Exp_Basics.v
@@ -2,19 +2,32 @@ From DAGs Require Import Basics.
 From DAGs Require Import CycleDetection.
 Import ListNotations.
 
+(*
+"Universal Assumptions:
+1. there's no measurement error
+2. all exogenous variables are mutually independent
+3. An experiment is measuring the treatment vector's effect on the reponse vector
+4. Every node (including unmeasurable) has a declared finite domain;
+    Unmeasurable restricts observation, not existence.
+5. dag's are finite
+
+"
+*)
+
+
 
 Inductive node_label : Type :=
   | Treatment
-  | Response
-  | Unmeasurable
-  | Unlabeled.
-(* the causal query in an experiment will be Treatment -> Response *)
+  | Response      (* sometimes a proxy *)
+  | Unmeasurable  (* nodes that can't be measured *)
+  | Unlabeled.    (* default label *)
+(* the causal query in an experiment will be list Treatment -> list Response *)
 
 Definition node_label_eqb (r1 r2 : node_label) : bool :=
   match r1, r2 with
   | Treatment,          Treatment          => true
   | Response,           Response           => true
-  | Unmeasurable,         Unmeasurable         => true (* nodes that can't be measured *)
+  | Unmeasurable,       Unmeasurable       => true
   | Unlabeled,          Unlabeled          => true
   | _,                  _                  => false
   end.
@@ -25,97 +38,106 @@ Proof. intros []; reflexivity. Qed.
 Lemma node_label_eqb_eq : forall r1 r2, node_label_eqb r1 r2 = true <-> r1 = r2.
 Proof. intros [] []; simpl; split; intro H; try reflexivity; try discriminate; try congruence. Qed.
 
-(* An aug_graph pairs an underlying causal DAG with a total function assigning each node a role.
-    Nodes not explicitly labeled by the researcher default to Unlabeled. *)
+(* specify size of the node domain so that we can abstract it to be [n] *)
+Definition node_card : Type := node -> nat.
+
+(* The values of node n's domain, as a list: [0; ...; dom n - 1] *)
+Definition node_dom (d : node_card) (n : node) : list nat := seq 0 (d n).
+
+
 Record aug_graph : Type := mk_aug_graph {
-  dag     : graph;
+  dag      : graph;
   label_of : node -> node_label;
+  dom      : node_card;
 }.
+
+(* Example: x -> y and x <- z -> y, where x:Treatment, y:Response.
+   Node ids: x = 1, y = 2, z = 3. z is an (unlabeled) confounder of x and y. *)
+Definition G_3 : aug_graph :=
+  mk_aug_graph
+    ([1; 2; 3], [(1, 2); (3, 1); (3, 2)])
+    (fun n => match n with
+              | 1 => Treatment
+              | 2 => Response
+              | _ => Unlabeled
+              end)
+    (fun n => match n with
+              | 1 => 2   (* x: values {0,1} = no treatment / treatment *)
+              | 2 => 3   (* y: values {0,1,2} = three outcome levels *)
+              | _ => 2   (* z (and any other node): binary *)
+              end).
 
 Definition nodes_with_label (ag : aug_graph) (r : node_label) : nodes :=
   filter (fun n => node_label_eqb (label_of ag n) r) (nodes_in_graph (dag ag)).
 
-Definition treatment_node (ag : aug_graph) : option node :=
-  match nodes_with_label ag Treatment with
-  | [t] => Some t
-  | _   => None
-  end.
+Definition treatment_nodes (ag : aug_graph) : nodes :=
+  nodes_with_label ag Treatment.
 
-Definition response_node (ag : aug_graph) : option node :=
-  match nodes_with_label ag Response with
-  | [r] => Some r
-  | _   => None
-  end.
+Definition response_nodes (ag : aug_graph) : nodes :=
+  nodes_with_label ag Response.
 
 Definition is_unmeasurable (ag : aug_graph) (n : node) : bool :=
   node_label_eqb (label_of ag n) Unmeasurable.
 
-Definition observed_nodes (ag : aug_graph) : nodes :=
+Definition measurable_nodes (ag : aug_graph) : nodes :=
   filter (fun n => negb (is_unmeasurable ag n)) (nodes_in_graph (dag ag)).
 
+(* a well-formed aug_graph contains >=1 treatment and response nodes,
+   and every node in the graph has a nonempty value domain *)
 Definition wf_aug_graph (ag : aug_graph) : Prop :=
   G_well_formed (dag ag) = true /\
   contains_cycle (dag ag) = false /\
-  length (nodes_with_label ag Treatment) = 1 /\
-  length (nodes_with_label ag Response)  = 1 /\
-  treatment_node ag <> response_node ag.
-(* "what if treatment/response are a function of other nodes.
-    should there be other node types, like proxy? or other edge types like definitional? " *)
+  treatment_nodes ag <> [] /\
+  response_nodes ag <> [] /\
+  (forall n, node_in_graph n (dag ag) = true -> 0 < dom ag n).
+
+Example wf_G_3 : wf_aug_graph G_3.
+Proof.
+  repeat split; try reflexivity; try discriminate.
+  intros n _. destruct n as [| [| [| n']]]; apply Nat.lt_0_succ.
+Qed.
 
 Lemma wf_has_treatment : forall ag,
   wf_aug_graph ag ->
-  exists t, treatment_node ag = Some t.
+  exists t, In t (treatment_nodes ag).
 Proof.
-  intros ag [_ [_ [Hlen _]]].
-  unfold treatment_node.
-  destruct (nodes_with_label ag Treatment) as [| t [| t' rest]] eqn:Heq.
-  - simpl in Hlen. discriminate.
-  - exists t. reflexivity.
-  - simpl in Hlen. discriminate.
+  intros ag [_ [_ [Hne _]]].
+  destruct (treatment_nodes ag) as [| t rest] eqn:Heq.
+  - contradiction.
+  - exists t. left. reflexivity.
 Qed.
 
 Lemma wf_has_response : forall ag,
   wf_aug_graph ag ->
-  exists r, response_node ag = Some r.
+  exists r, In r (response_nodes ag).
 Proof.
-  intros ag [_ [_ [_ [Hlen _]]]].
-  unfold response_node.
-  destruct (nodes_with_label ag Response) as [| r [| r' rest]] eqn:Heq.
-  - simpl in Hlen. discriminate.
-  - exists r. reflexivity.
-  - simpl in Hlen. discriminate.
+  intros ag [_ [_ [_ [Hne _]]]].
+  destruct (response_nodes ag) as [| r rest] eqn:Heq.
+  - contradiction.
+  - exists r. left. reflexivity.
 Qed.
 
-Inductive operation (X : Type) : Type :=
-  | Intervene (n : node) (v : X)   (* do(n=v): set n to a specific value *)
-  | Randomize (n : node) (r : node)      (* RCT: remove incoming edges to n, add fresh r → n *)
-(*  | Control  (n : node) (v : X)  *)
+(* Values are nat codes in 0 .. dom n - 1 (Universal Assumption 4). *)
+Inductive operation : Type :=
+  | Intervene (n : node) (v : nat)   (* do(n=v): set n to a specific value *)
+  | Randomize (n : node) (r : node)  (* RCT: remove incoming edges to n, add fresh r → n *)
+(*  | Control  (n : node) (v : nat)  *)
   | Measure   (n : node).            (* record n's current value into the log *)
   (* | Wait. *)
 
-Arguments Intervene {X}.
-Arguments Randomize {X}.
-(* Arguments Stratify  {X}. *)
-Arguments Measure   {X}.
-(* Arguments Wait      {X}. *)
+Definition program : Type := list operation.
 
-(* temporary name until I found a better name *)
-Definition program (X : Type) : Type := list (operation X).
-
-Definition wf_operation {X : Type} (ag : aug_graph) (op : operation X) : Prop :=
+Definition wf_operation (ag : aug_graph) (op : operation) : Prop :=
   match op with
-  | Intervene n _ =>
-      node_in_graph n (dag ag) = true
+  | Intervene n v =>
+      node_in_graph n (dag ag) = true /\ v < dom ag n
   | Randomize n r =>
-      node_in_graph n (dag ag) = true /\
-      node_in_graph r (dag ag) = false
-  (* | Stratify n _ =>
-      node_in_graph n (dag ag) = true*)
+      node_in_graph n (dag ag) = true /\ node_in_graph r (dag ag) = false
+  (* | Stratify n _ => node_in_graph n (dag ag) = true*)
   | Measure n =>
-      node_in_graph n (dag ag) = true /\
-      label_of ag n <> Unmeasurable
+      node_in_graph n (dag ag) = true /\ label_of ag n <> Unmeasurable
   (* | Wait => True *)
   end.
 
-Definition wf_program {X : Type} (ag : aug_graph) (prog : program X) : Prop :=
+Definition wf_program (ag : aug_graph) (prog : program) : Prop :=
   Forall (wf_operation ag) prog.
--- ProbabilityExtension/Probability.v
+++ ProbabilityExtension/Probability.v
@@ -1,25 +1,23 @@
 From DAGs Require Import Basics_Constr.
 From CausalDiagrams Require Import Assignments.
+From Experiment Require Import Exp_Basics.
 From Stdlib Require Import Reals.
 
 Import ListNotations.
 Open Scope R_scope.
 
-(* A finite space of values (simplified to nat) each node can take on *)
-Definition node_domains : Type := node -> list nat.
-
 (* A world is a complete assignment where each node gets a value from its domain *)
-Definition world (dom : node_domains) (G : graph) : Type :=
+Definition world (dom : node_card) (G : graph) : Type :=
   { A : assignments nat |
       is_exact_assignment_for A (nodes_in_graph G)
-    /\ forall u val, In (u, val) A -> In val (dom u) }.
+    /\ forall u val, In (u, val) A -> (val < dom u)%nat }.
 
 (* Enumerate all assignments where each node gets a value from its domain *)
-Fixpoint enum_assignments (dom : node_domains) (V : nodes) : list (assignments nat) :=
+Fixpoint enum_assignments (dom : node_card) (V : nodes) : list (assignments nat) :=
   match V with
   | [] => [ [] ]
   | u :: rest =>
-      flat_map (fun res_assignments => map (fun val => (u, val) :: res_assignments) (dom u))
+      flat_map (fun res_assignments => map (fun val => (u, val) :: res_assignments) (node_dom dom u))
                (enum_assignments dom rest)
   end.
 
@@ -30,12 +28,12 @@ Local Open Scope nat_scope.
 (* Example: graph 1->2->3, dom 1 = {0}, dom 2 = {0,1}, dom 3 = {0,1} *)
 Definition G_ex : graph := ([1;2;3],[(1,2);(2,3)]).
 
-Definition dom_ex : node_domains := fun n =>
+Definition dom_ex : node_card := fun n =>
   match n with
-  | 1 => [0]
-  | 2 => [0; 1]
-  | 3 => [0; 1]
-  | _ => []
+  | 1 => 1
+  | 2 => 2
+  | 3 => 2
+  | _ => 0
   end.
 
 Example enum_ex :
@@ -49,11 +47,11 @@ Proof. reflexivity. Qed.
 End Examples.
 
 (* Correctness: enum_assignments produces exactly the valid complete assignments *)
-Theorem enum_assignments_correct (dom : node_domains) (G : graph) :
+Theorem enum_assignments_correct (dom : node_card) (G : graph) :
   forall A : assignments nat,
   In A (enum_assignments dom (nodes_in_graph G))
   <->
-  is_exact_assignment_for A (nodes_in_graph G) /\ forall u val, In (u, val) A -> In val (dom u).
+  is_exact_assignment_for A (nodes_in_graph G) /\ forall u val, In (u, val) A -> (val < dom u)%nat.
 Proof. Admitted.
 
 Fixpoint map_with_In {A B : Type} (l : list A) (f : forall a, In a l -> B) : list B :=
@@ -64,15 +62,15 @@ Fixpoint map_with_In {A B : Type} (l : list A) (f : forall a, In a l -> B) : lis
       map_with_In t (fun a Ha => f' a (in_cons h a t Ha))
   end f.
 
-Definition enum_worlds (dom : node_domains) (G : graph) : list (world dom G) :=
+Definition enum_worlds (dom : node_card) (G : graph) : list (world dom G) :=
   let As := enum_assignments dom (nodes_in_graph G) in
   map_with_In As (fun A HA => exist _ A (proj1 (enum_assignments_correct dom G A) HA)).
 
 
-Theorem enum_worlds_sound (dom : node_domains) (G : graph) :
+Theorem enum_worlds_sound (dom : node_card) (G : graph) :
   forall w, In w (enum_worlds dom G) <->
   is_exact_assignment_for (proj1_sig w) (nodes_in_graph G)
-  /\ forall u val, In (u, val) (proj1_sig w) -> In val (dom u).
+  /\ forall u val, In (u, val) (proj1_sig w) -> (val < dom u)%nat.
 Proof.
     constructor.
     - intros. exact (proj2_sig w).
@@ -82,15 +80,15 @@ Admitted.
 Definition Rsum (l : list R) : R := fold_right Rplus 0 l.
 
 (* An event is a set of worlds, represented as a characteristic function *)
-Definition event (dom : node_domains) (G : graph) : Type := world dom G -> bool.
+Definition event (dom : node_card) (G : graph) : Type := world dom G -> bool.
 
 (* Probability of an event: sum pmf over all worlds in the event *)
-Definition prob_event {dom : node_domains} {G : graph}
+Definition prob_event {dom : node_card} {G : graph}
     (P : (world dom G -> R)) (E : event dom G) : R :=
   Rsum (map P (filter E (enum_worlds dom G))).
 
 (* A probability measure over worlds satisfying non-negativity, normalization, and additivity *)
-Record prob_measure (dom : node_domains) (G : graph) : Type := mkProbMeasure {
+Record prob_measure (dom : node_card) (G : graph) : Type := mkProbMeasure {
   pmf :> world dom G -> R;
   pmf_nonneg     : forall w : world dom G, 0 <= pmf w;
   pmf_normalized : Rsum (map pmf (enum_worlds dom G)) = 1;
